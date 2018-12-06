fae5b7e7ac658f47ae898e38d577d113da3f1f09
Shakapark <shakapark1994@gmail.com>
2018-12-06 10:59:44 +0100

    Add Prometheus Plugin

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,4 +12,5 @@ if [ -z ${FLUENT_ELASTICSEARCH_PASSWORD} ] ; then
    sed -i  '/FLUENT_ELASTICSEARCH_PASSWORD/d' /fluentd/etc/${FLUENTD_CONF}
 fi
 
+fluent-gem install fluent-plugin-prometheus --version='~>1.0.0'
 exec fluentd -c /fluentd/etc/${FLUENTD_CONF} -p /fluentd/plugins --gemfile /fluentd/Gemfile ${FLUENTD_OPT}
