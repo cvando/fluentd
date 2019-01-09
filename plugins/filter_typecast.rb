aed31d6bde57979c6e9827b79c77ee63bf86eaf6
cvando <clement.vandoolaeghe@gmail.com>
2019-01-09 16:53:39 +0100

    Create filter_typecast.rb

---
+++ plugins/filter_typecast.rb
@@ -0,0 +1,17 @@
+require 'fluent/filter'
+require 'fluent/parser'
+
+module Fluent
+  class TypecastFilter < Filter
+    Fluent::Plugin.register_filter('typecast', self)
+
+    include ::Fluent::TextParser::TypeConverter
+
+    def filter(tag, time, record)
+      filtered = record.map do |key, val|
+        [key, convert_type(key, val)]
+      end
+      Hash[*filtered.flatten(1)]
+    end
+  end
+end
